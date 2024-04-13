b1c340f9b109afd479f742f56809b1b30cdd53c7
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-04-13 17:47:21 +0530

    Add write

---
+++ Client/src/component/SOCIAL-MEDIA/Socialmedia.jsx
@@ -0,0 +1,33 @@
+import "./social.css"
+
+function Socialmedia() {
+  return (
+    <div className="social-media">
+      <div className="categories">
+        <h3>CATEGORIES<div className="line" /></h3>
+        <div className="categorie">
+          <h4>LIFESTYLE</h4>
+          <p>402</p>
+        </div>
+        <div className="categorie">
+          <h4>FASHION</h4>
+          <p>213</p>
+        </div>
+        <div className="categorie">
+          <h4>TECHNOLOGY</h4>
+          <p>311</p>
+        </div>
+        <div className="categorie">
+          <h4>TRAVEL</h4>
+          <p>56</p>
+        </div>
+        <div className="categorie">
+          <h4>HEALTH</h4>
+          <p>102</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Socialmedia
